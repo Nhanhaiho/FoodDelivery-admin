4cbf01b1cfee1e3fe2fa3e95664fbfafc48c7ea2
petedaogit <namdao.0908@gmail.com>
2025-01-18 17:22:45 +0700

    edit buttons and update url

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-be-xk4s.onrender.com";
 
   return (
     <div>
